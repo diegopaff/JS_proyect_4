7896306d2318bcc62fd32fe51509fcd4e7994445
diegopaff <110573986+diegopaff@users.noreply.github.com>
2023-10-08 21:43:22 -0300

    babel and webpack

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude:/node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    }
+};
